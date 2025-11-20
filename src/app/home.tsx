0e654d8ae4e78aa7470063b70539ea196efd1b49
thiagof00 <thiagoribeiro0302@gmail.com>
2025-11-19 22:13:26 -0300

    Refatoramento do componente de animação do botão SOS da home e inicio do botões secundários

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,89 +1,38 @@
-import React, { useEffect, useRef } from "react";
+import { router } from "expo-router";
+import React from "react";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  Animated,
   StyleSheet,
+  Text,
+  TouchableHighlight
 } from "react-native";
-
-const pinkBase = "#ff4f7d";
+import ViewAnimatedButton from "./components/ViewAnimatedButton";
+import { Body } from "./global";
+import { ButtonSoS, Container, FooterHome, TextButtonSoS } from "./styles/home";
 
 export default function Home () {
-  const pulse1 = useRef(new Animated.Value(0)).current;
-  const pulse2 = useRef(new Animated.Value(0)).current;
-
-  const createPulse = (anim: Animated.Value): Animated.CompositeAnimation => {
-    return Animated.loop(
-      Animated.sequence([
-        Animated.timing(anim, {
-          toValue: 1,
-          duration: 2000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(anim, {
-          toValue: 0,
-          duration: 0,
-          useNativeDriver: true,
-        }),
-      ])
-    );
-  };
-
-  useEffect(() => {
-    createPulse(pulse1).start();
-    setTimeout(() => createPulse(pulse2).start(), 700);
-  }, []);
-
+  
   return (
-    <View style={styles.wrapper}>
-      {/* Animation 1 */}
-      <Animated.View
-        style={[
-          styles.pulseCircle,
-          {
-            transform: [
-              {
-                scale: pulse1.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [1, 2.5],
-                }),
-              },
-            ],
-            opacity: pulse1.interpolate({
-              inputRange: [0, 1],
-              outputRange: [0.7, 0],
-            }),
-          },
-        ]}
-      />
+    <Body>
+      
+      <Container>
 
-      {/* Animation 2 */}
-      <Animated.View
-        style={[
-          styles.pulseCircle,
-          {
-            transform: [
-              {
-                scale: pulse2.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [1, 2.5],
-                }),
-              },
-            ],
-            opacity: pulse2.interpolate({
-              inputRange: [0, 1],
-              outputRange: [0.7, 0],
-            }),
-          },
-        ]}
-      />
+      <ViewAnimatedButton colorBackground="#FFB3C6" outputRange={[1,1.4]}/>
+      <ViewAnimatedButton colorBackground="#FF8FAB" outputRange={[1,2]}/>
+      <ButtonSoS onPress={()=>router.push("/")}>
+        <TextButtonSoS>SOS</TextButtonSoS>
+      </ButtonSoS>
+     
+      </Container>
 
-      {/* Button */}
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.text}>SOS</Text>
-      </TouchableOpacity>
-    </View>
+      <FooterHome>
+      <TouchableHighlight>
+        <Text style={{backgroundColor:"#FF8FAB", width: 120, height:48,color:"#FFF", textAlign:"center", alignItems:"center", marginRight: 24}}>Fazer Ligação</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={{backgroundColor:"#FF8FAB", width: 120, height:48,color:"#FFF", textAlign:"center", alignItems:"center", }}>Localização</Text>
+      </TouchableHighlight>
+      </FooterHome>
+    </Body>
   );
 };
 
@@ -91,37 +40,7 @@ export default function Home () {
 
 const styles = StyleSheet.create({
   wrapper: {
-    justifyContent: "center",
-    alignItems: "center",
+    
   },
 
-  pulseCircle: {
-    position: "absolute",
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    backgroundColor: pinkBase,
-  },
-
-  button: {
-    width: 140,
-    height: 140,
-    borderRadius: 70,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: pinkBase,
-
-    shadowColor: pinkBase,
-    shadowOpacity: 0.4,
-    shadowRadius: 20,
-    shadowOffset: { width: 0, height: 0 },
-    elevation: 10,
-  },
-
-  text: {
-    color: "white",
-    fontSize: 30,
-    fontWeight: "bold",
-  },
 });
-
---
+++ src/app/styles/home.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native'
+
+export const ButtonSoS = styled.TouchableOpacity`
+
+    width: 140px;
+    height: 140px;
+    border-radius: 70px;
+    justify-content: center;
+    align-items: center;
+    background-color: #FB6F92;
+`
+
+export const TextButtonSoS = styled.Text`
+
+    color: #FFF;
+    font-size: 30px;
+    font-weight: bold;
+`
+export const Container = styled.View`
+    width: 100%;
+    height: 70%;
+
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+`
+
+export const FooterHome = styled.View`
+    width: 100%;
+    height: 30%;
+
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+
+`
